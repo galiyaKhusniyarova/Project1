6ecf4a82235e5fb6bfaef0219d977fe435e6a2e1
galiyaKhusniyarova <111472420+galiyaKhusniyarova@users.noreply.github.com>
2022-10-19 21:24:53 -0700

    updated the res_csv

--- sql_CLI.py
+++ sql_CLI.py
@@ -22,19 +22,54 @@ def read_table(engine, table_name):
     print(f"{table_name} Data:")
     print(results_dataframe)
     
+"""CREATE
+
+The CREATE operation creates a new table in the database using the given DataFrame.
+The table is replaced by the new data if it already exists.
+"""
+
+
+def create_table(engine, table_name, table_data_df):
+    table_data_df.to_sql(table_name, engine, index=True, if_exists='replace')
+    
+"""SELECT"""
+
+
+def select_data(engine, table_name):
+    select_sql = """
+    SELECT {table_name} from dcf_all
+    """
     
 
 def run():
-    # Create a dataframe
-    esg_df = pd.read_csv('./resources/esg_data.csv')
-    sp_500_df = pd.read_csv('./resources/SP500_stocks.csv')
-
-    #Create tables
-    esg_df.to_sql("esg", engine, index=False, if_exists='replace')
-    sp_500_df.to_sql("sp_500", engine, index = False, if_exists='replace')
-
-    table_name = questionary.text("Which table would you like to read?").ask()
-    read_table(engine, table_name)
+    
+    res_df = pd.read_csv('./FAANG_DATA/res.csv')
+    res_df.set_index('symbol', inplace = True)
+    res_df.to_csv('res2.csv')
+    
+    #Create original table
+    create_table(engine, 'dcf_all', res_df)
+    
+    
+    sql_cli_running = True
+    
+    while sql_cli_running:
+        choice = questionary.select(
+            "What do you want to do?",
+            choices=["Read", "Merge", "Save", "Quit"]
+        ).ask()
+        
+        if choice == "Read":
+            table_name = questionary.select(
+                "Which table would you like to read?",
+                choices = ["META", "AAPL", "AMZN", "GOOG"]
+            ).ask()
+            if table_name == "META":
+                select_data(engine, table_name)
+                read_table(engine, table_name)
+        elif choice == "Quit":
+            sql_cli_running = False
+            print("Goodbye")
 
 if __name__ == "__main__":
     fire.Fire(run)
